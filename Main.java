9f4f97b1495684be1a74740c2863b1506993a63e
Nick Collins <n0488629@actx.edu>
2025-03-28 13:23:32 -0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;  // Import the Scanner class
+
+class Main {
+public static void castVote(int[] Can1, int[] Can2, int[] Can3){
+    Scanner myObj1 = new Scanner(System.in);
+  System.out.println("Which candidate do you want to vote for? 1: Candidate 1 2: Candidate 2 3: Candidate 3.");
+  int userchoice = myObj1.nextInt();
+
+  switch(userchoice) {
+    case 1:
+    Can1[0] = Can1[0] + 1;
+    System.out.println("Vote successfully cast.");
+    break;
+    case 2:
+    Can2[0] = Can2[0] + 1;
+    System.out.println("Vote successfully cast.");
+
+    break;
+    case 3:
+    Can3[0] = Can3[0] + 1;
+    System.out.println("Vote successfully cast.");
+    break;
+    default:
+    System.out.println("Incorrect option entered.");
+  }
+  
+}
+
+  public static void resetVotes(int[] Can1, int[] Can2, int[] Can3){
+    Can1[0] = 0;
+    Can2[0] = 0;
+    Can3[0] = 0;
+    System.out.println("Votes have been reset");
+  }
+  public static void displayResults(int[] Can1, int[] Can2, int[] Can3){
+    System.out.println(" Votes for candidate 1: " + Can1[0]);
+    System.out.println(" Votes for candidate 2: " + Can2[0]);
+    System.out.println(" Votes for candidate 3: " + Can3[0]);
+  }
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    int[] Can1 = {0};
+    int[] Can2 = {0};
+    int[] Can3 = {0};
+    System.out.println("Voting System Program");
+    
+    boolean goagain = true;
+    while (goagain) {
+        
+    System.out.println("Enter a number to choose an operation: 1: CastVote 2: DisplayResults 3: ResetVotes 4: Exit ");
+    int userinput = myObj.nextInt();
+    switch (userinput) {
+        case 1:
+        castVote(Can1, Can2, Can3);
+        break;
+        case 2:
+        displayResults(Can1, Can2, Can3);
+        break;
+        case 3:
+        resetVotes(Can1, Can2, Can3);
+        break;
+        default:
+        System.out.println("Thank you for using the program");
+        goagain = false;
+    }
+}
+    
+
+   
+
+
+
+
+
+
+
+
+
+    myObj.close();
+  }
+}
